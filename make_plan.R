a32155f024c58c544f8eec2f4a8989e000b58eba
Robert M Flight <r.flight79@gmail.com>
2018-06-27 16:28:12 -0400

    moving the file location so everything is just relative file paths

--- make_plan.R
+++ make_plan.R
@@ -4,17 +4,17 @@ library(rprojroot)
 library(furrr)
 plan(multiprocess)
 project_root <- find_root(is_rstudio_project)
-source(file.path(project_root, "data_analysis/R/processing_functions.R"))
+source("data_analysis/R/processing_functions.R")
 
 peak_pkg_description = utils::packageDescription('FTMS.peakCharacterization')
 
 setup_100cpos <- drake_plan(
-  mzml_100cpos = file_in(file.path(project_root, 'data_analysis/data_input/100Cpos/100Cpos.mzML')),
+  mzml_100cpos = file_in('data_analysis/data_input/100Cpos/100Cpos.mzML'),
   tiled_100cpos = average_scans_tile_windows(mzml_100cpos, peak_pkg_description)
 )
 
 setup_ecf <- drake_plan(
-  mzml_ecf = file_in(file.path(project_root, 'data_analysis/data_input/ecf_data/7_HsPlt_13Cglc_p5Uthrombin_160728_polar_ECF_2.mzML')),
+  mzml_ecf = file_in('data_analysis/data_input/ecf_data/7_HsPlt_13Cglc_p5Uthrombin_160728_polar_ECF_2.mzML'),
   tiled_ecf = average_scans_tile_windows(mzml_ecf, peak_pkg_description)
 )
 
