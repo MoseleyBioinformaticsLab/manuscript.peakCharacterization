6361e9a4ae40cf5608752464f7b10659bea2a0b5
Robert Flight <r.flight79@gmail.com>
2018-06-27 15:34:14 -0400

    was trying to test something here

--- data_analysis/make_plan.R
+++ data_analysis/make_plan.R
@@ -11,12 +11,12 @@ peak_picking_plan <- drake_plan(
   zip_data = raw_peakpicking(zip_file, peak_pkg_description)
 )
 
-noise_and_offsets_plan <- drake_plan(
-  keep_noise_no_offset = keep_noise_no_offset_correspondence(zip_data),
-  remove_noise_no_offset = remove_noise_remove_noise_no_offset_correspondence(zip_data),
-  remove_noise_do_offset = remove_noise_do_offset_correspondence(zip_data)
-)
+#noise_and_offsets_plan <- drake_plan(
+#  keep_noise_no_offset = keep_noise_no_offset_correspondence(zip_data),
+#  remove_noise_no_offset = remove_noise_remove_noise_no_offset_correspondence(zip_data),
+#  remove_noise_do_offset = remove_noise_do_offset_correspondence(zip_data)
+#)
 
-full_plan <- rbind(peak_picking_plan, noise_and_offsets_plan)
+full_plan <- rbind(peak_picking_plan)#, noise_and_offsets_plan)
 
-make(full_plan, parallelism = "mclapply_staged", jobs = 10)
+make(full_plan, parallelism = "mclapply", jobs = 10)
