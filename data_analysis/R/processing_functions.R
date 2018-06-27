3b1b815b579060aad09cefeb5a5a9aaca2026638
Robert M Flight <r.flight79@gmail.com>
2018-06-27 15:31:57 -0400

    new running just a single file to see how this goes

--- data_analysis/R/processing_functions.R
+++ data_analysis/R/processing_functions.R
@@ -1,100 +1,18 @@
-filter_scans <- function(raw_data){
-  scan_times <- data.frame(scan = raw_data$scan_range,
-                           time = raw_data$raw_data@scantime[raw_data$scan_range])
-  scan_times <- dplyr::mutate(scan_times, lag = time - dplyr::lag(time), lead = dplyr::lead(time) - time)
-  na_keep <- (is.na(scan_times$lag) | is.na(scan_times$lead)) & ((scan_times$lag >= 4) | (scan_times$lead >= 4))
-  na_keep <- na_keep & !is.na(na_keep)
-  gt_4_keep <- (scan_times$lag >= 4) & (scan_times$lead >= 4)
-  scan_times <- scan_times[(na_keep | gt_4_keep), ]
+# common setup stuff
+#
+# average the scans and generate the sliding and tiling windows
+average_scans_tile_windows <- function(in_file, pkg_description){
+  char_ms <- CharacterizeMSPeaks$new(in_file, peak_finder = PeakRegionFinder$new())
+  char_ms$load_file()
+  set_internal_map(furrr::future_map)
+  char_ms$filter_raw_scans()
+  char_ms$zip_ms$peak_finder <- char_ms$peak_finder
+  char_ms$zip_ms$peak_finder$add_data(char_ms$zip_ms$raw_ms)
+  char_ms$zip_ms$peak_finder$sample_id <- char_ms$zip_ms$id
+  char_ms$zip_ms$cleanup()
+  char_ms$zip_ms$peak_finder$add_sliding_regions()
+  char_ms$zip_ms$peak_finder$add_tiled_regions()
 
-  raw_data$set_scans(scan_range = scan_times$scan)
-  raw_data
+  char_ms
 }
 
-
-raw_peakpicking <- function(zip_file, pkg_description){
-  zip_data <- zip_ms(zip_file)
-  zip_data$load_raw()
-  zip_data$cleanup()
-  zip_data$peak_finder <- PeakFinder$new(raw_filter = filter_scans)
-
-  zip_data$peak_finder$raw_data <- zip_data$raw_ms
-
-  zip_data$peak_finder$apply_raw_filter()
-  #self$create_multi_scan()
-  zip_data$peak_finder$create_multi_scan_peaklist()
-  zip_data$peak_finder$filter_dr_models()
-
-  zip_data$peak_finder$create_multi_scan_peaklist()
-  zip_data$peak_finder$multi_scan_peaklist$calculate_average_mz_model()
-  zip_data
-}
-
-# identity offset correction, i.e. do no offset correction
-# because we want to keep lots of other information, we are just replacing
-# the correction factor with 0, and subtracting that out
-identity_correct_offset_function <- function(master_peak_list, multi_scan_peaklist, min_scan = 0.1){
-
-  n_col <- ncol(master_peak_list$scan_mz)
-  n_min_scan <- SIRM.FTMS.peakCharacterization:::correctly_round_numbers(n_col, min_scan)
-  correspond_peaks <- master_peak_list$count_notna() >= n_min_scan
-
-  scan_indices <- master_peak_list$scan_indices
-
-  multi_scan_peaklist_corrected <- multi_scan_peaklist$clone(deep = TRUE)
-
-  offset_models <- vector(mode = "list", length = length(scan_indices))
-
-  for (iscan in seq(1, n_col)) {
-    scan_offset <- master_peak_list$scan_mz[correspond_peaks, iscan] -
-      master_peak_list$master[correspond_peaks]
-    offset_model <- master_peak_list$offset_fit_function(master_peak_list$master[correspond_peaks], scan_offset)
-
-    use_scan <- scan_indices[iscan]
-    tmp_peaks <- multi_scan_peaklist_corrected$peak_list_by_scans[[use_scan]]$peak_list
-    use_mz <- tmp_peaks$ObservedMZ
-    offset_predict <- 0
-    corrected_mz <- use_mz - offset_predict
-    tmp_peaks$ObservedMZ <- corrected_mz
-    multi_scan_peaklist_corrected$peak_list_by_scans[[use_scan]]$peak_list <- tmp_peaks
-    offset_model$scan_index <- scan_indices[iscan]
-    offset_models[[iscan]] <- offset_model
-  }
-
-  list(multi_scan_peaklist = multi_scan_peaklist_corrected,
-       models = offset_models)
-}
-
-keep_noise_no_offset_correspondence <- function(zip_data){
-  zip_data2 <- zip_data$clone(deep = TRUE)
-  zip_data2$raw_ms <- NULL
-  zip_data2$peak_finder$multi_scan_peaklist$peak_list_by_scans <- purrr::map(zip_data2$peak_finder$multi_scan_peaklist$peak_list_by_scans,
-                                                                             function(x){
-                                                                               x$peak_list$not_noise <- TRUE
-                                                                               x
-                                                                             })
-  zip_data2$peak_finder$keep_all_master_peak_lists <- TRUE
-  zip_data2$peak_finder$keep_intermediates <- TRUE
-  zip_data$peak_finder$offset_correction_function <- identity_correct_offset_function
-  zip_data2$peak_finder$create_correspondent_peaks()
-  zip_data2
-}
-
-remove_noise_no_offset_correspondence <- function(zip_data){
-  zip_data2 <- zip_data$clone(deep = TRUE)
-  zip_data2$raw_ms <- NULL
-  zip_data$peak_finder$offset_correction_function <- identity_correct_offset_function
-  zip_data2$peak_finder$keep_all_master_peak_lists <- TRUE
-  zip_data2$peak_finder$keep_intermediates <- TRUE
-  zip_data2$peak_finder$create_correspondent_peaks()
-  zip_data2
-}
-
-remove_noise_do_offset_correspondence <- function(zip_data){
-  zip_data2 <- zip_data$clone(deep = TRUE)
-  zip_data2$raw_ms <- NULL
-  zip_data2$peak_finder$keep_all_master_peak_lists <- TRUE
-  zip_data2$peak_finder$keep_intermediates <- TRUE
-  zip_data2$peak_finder$create_correspondent_peaks()
-  zip_data2
-}
--- data_analysis/make_plan.R
+++ data_analysis/make_plan.R
@@ -1,22 +1,16 @@
 library(drake)
-library(SIRM.FTMS.peakCharacterization)
+library(FTMS.peakCharacterization)
 library(rprojroot)
+library(furrr)
+plan(multiprocess)
 project_root <- find_root(is_rstudio_project)
 source(file.path(project_root, "data_analysis/R/processing_functions.R"))
 
-peak_pkg_description = utils::packageDescription('SIRM.FTMS.peakCharacterization')
+peak_pkg_description = utils::packageDescription('FTMS.peakCharacterization')
 
-peak_picking_plan <- drake_plan(
-  zip_file = file_in(file.path(project_root, 'data_analysis/data_input/100Cpos.zip')),
-  zip_data = raw_peakpicking(zip_file, peak_pkg_description)
+setup_100cpos <- drake_plan(
+  mzml_100cpos = file_in(file.path(project_root, 'data_analysis/data_input/100Cpos/100Cpos.mzML')),
+  tiled_100cpos = average_scans_tile_windows(mzml_100cpos, peak_pkg_description)
 )
 
-noise_and_offsets_plan <- drake_plan(
-  keep_noise_no_offset = keep_noise_no_offset_correspondence(zip_data),
-  remove_noise_no_offset = remove_noise_remove_noise_no_offset_correspondence(zip_data),
-  remove_noise_do_offset = remove_noise_do_offset_correspondence(zip_data)
-)
-
-full_plan <- rbind(peak_picking_plan, noise_and_offsets_plan)
-
-make(full_plan, parallelism = "mclapply_staged", jobs = 10)
+make(setup_100cpos)
