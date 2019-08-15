f714db2509f2cd51d86b93b542ded42f64b3e529
rmflight <r.flight79@gmail.com>
2019-08-15 10:56:44 -0400

    using cross and combine so we will easily be able to compare across methods applied to a particular dataset

--- make_plan.R
+++ make_plan.R
@@ -25,59 +25,37 @@ write_loc = "data_analysis/data_output"
 analysis_plan = drake_plan(
   # check the version of peak characterization we are using, so if it changes, we rerun
   pkg = peak_pkg_description,
+  # read in and prep each of the mzML files
   data = target(
     file_in(input) %>% reading_scans_tile_windows(., pkg),
     transform = map(input = !!use_files, .id = FALSE)
   ),
-  group1_method = target(
-    group1_characterization(data),
-    transform = map(data)
-  ),
-  group2_method = target(
-    group2_characterization(data),
-    transform = map(data)
-  ),
-  group3_method = target(
-    group3_characterization(data),
-    transform = map(data)
-  ),
-  group4_method = target(
-    group4_characterization(data),
-    transform = map(data)
-  ),
-  group5_method = target(
-    group5_characterization(data),
-    transform = map(data)
-  ),
-  group6_method = target(
-    group6_characterization(data),
-    transform = map(data)
-  ),
-  write_groups = target(
-    write_peaks_for_assignment(group6_method),
-    transform = map(group6_method)
-  ),
-  assign_groups = target(
-    assign_files(write_groups),
-    transform = map(write_groups)
-  ),
-  raw_assignments = target(
-    read_assignments(assign_groups),
-    transform = map(assign_groups)
-  ),
-  interesting_peaks = target(
-    find_interesting_peaks(raw_assignments),
-    transform = map(raw_assignments)
+  # for each of the ways we could process the data, run it on each one of the
+  # prepped data files
+  method = target(
+    method_fun(data),
+    transform = cross(method_fun = c(noperc_nonorm,
+                                     perc99_nonorm,
+                                     singlenorm,
+                                     singlenorm_int,
+                                     doublenorm,
+                                     filtersd), data)
   ),
   frequency_conversion = target(
     plot_frequency_conversion(data)
   ),
   peak_ordering = target(
-    plot_peak_ordering(group6_method_data)
+    plot_peak_ordering(method_filtersd_data)
   ),
   sliding_regions = target(
     plot_sliding_window_density(data)
   )
+  # # here we want to combine the ways a data file was processed and compare
+  # # their rsd's across processing methods.
+  # rsd_data = target(
+  #   rsd_info(method),
+  #   transform = combine(method, .by = data)
+  # )
 )
 
 make(analysis_plan, lock_envir = FALSE, memory_strategy = "memory")
