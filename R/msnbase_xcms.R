8ea4b49080730e8308c175b5dcb5e30a50d27caa
rmflight <r.flight79@gmail.com>
2022-03-10 16:27:06 -0500

    if we don't do scan time filtering, we will be bit

--- R/msnbase_xcms.R
+++ R/msnbase_xcms.R
@@ -1,5 +1,6 @@
 msnbase_centroid = function(mzml_file, char_list = NULL){
 
+
   if (!is.null(char_list)) {
     # get the scans from the normalization object,
     # and then subset the profiled scans using it
@@ -7,8 +8,11 @@ msnbase_centroid = function(mzml_file, char_list = NULL){
 
   mzml_prof = MSnbase::readMSData(mzml_file, msLevel. = 1, centroided. = FALSE)
   mzml_info = get_ms_info(mzml_prof)
+  mzml_info_hitime = msnbase_time_filter(mzml_info, 4)
 
-  all_scans_cent = mzml_prof %>%
+  mzml_prof2 = mzml_prof[mzml_info_hitime$scan]
+
+  all_scans_cent = mzml_prof2 %>%
     MSnbase::pickPeaks()
   all_scans_cent_mz = mz(all_scans_cent)
   all_scans_cent_intensity = intensity(all_scans_cent)
@@ -19,7 +23,7 @@ msnbase_centroid = function(mzml_file, char_list = NULL){
                scan = in_scan)
   })
 
-  comb_prof = MSnbase::combineSpectra(mzml_prof, method = meanMzInts, mzd = 0, ppm = 1)
+  comb_prof = MSnbase::combineSpectra(mzml_prof2, method = meanMzInts, mzd = 0, ppm = 1)
 
   comb_cent = comb_prof %>%
     MSnbase::pickPeaks()
@@ -27,4 +31,19 @@ msnbase_centroid = function(mzml_file, char_list = NULL){
                               intensity = intensity(comb_cent)[[1]])
 
   list(scanlevel = all_scans_data, comb = comb_cent_data)
+}
+
+msnbase_time_filter <- function(scan_times, min_time_difference = 4){
+  scan_times <- dplyr::mutate(scan_times, lag = rtime - dplyr::lag(rtime), lead = dplyr::lead(rtime) - rtime)
+
+  high_lag <- scan_times$lag >= min_time_difference
+  high_lag[is.na(high_lag)] <- TRUE
+  high_lead <- scan_times$lead >= min_time_difference
+  high_lead[is.na(high_lead)] <- TRUE
+
+  na_lead_high_lag <- is.na(scan_times$lead) & high_lag
+  na_lag_high_lead <- is.na(scan_times$lag) & high_lead
+
+  keep_scans <- (na_lead_high_lag | high_lag) & (na_lag_high_lead | high_lead)
+  scan_times[keep_scans, ]
 }
