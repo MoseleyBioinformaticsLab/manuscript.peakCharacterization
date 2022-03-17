344a8e0718c9565376db2367a50953637e1ffb99
rmflight <r.flight79@gmail.com>
2022-03-17 14:36:53 -0400

    generating the merged and peak-picked samples across all of lung

--- lungcancer_all/msnbase_peakset.R
+++ lungcancer_all/msnbase_peakset.R
@@ -1,5 +1,6 @@
-library(MSnbase)
-source("./R/msnbase_xcms.R")
+source("./packages.R")
+lapply(list.files(here::here("./R"), full.names = TRUE), source)
+library(knitrProgressBar)
 all_mzml_files <- dir("/mlab/data/archives/FTMS_raw/CESB/mzml_data", recursive = TRUE, full.names = TRUE, pattern = "mzML$")
 names(all_mzml_files) = gsub(".mzML$", "", basename(all_mzml_files))
 
@@ -10,16 +11,11 @@ zip_mzml = intersect(names(all_mzml_files), names(all_zip_files))
 
 all_mzml_files = all_mzml_files[zip_mzml]
 
+pb = knitrProgressBar::progress_estimated(length(all_mzml_files))
 msnbase_peaks = purrr::imap(all_mzml_files, function(in_mzml, id_mzml){
-  msnbase_centroid(in_mzml, sample_id = id_mzml)
-})
-
-sample_id = gsub("_peaks.json", "", basename(all_json))
-json_peaks = purrr::map(all_json, function(.x){
-  tmp = jsonlite::fromJSON(.x)$Peaks
-  sample = gsub("_peaks.json", "", basename(.x))
-  tmp$sample = sample
+  tmp = msnbase_centroid(in_mzml, sample_id = id_mzml)
+  knitrProgressBar::update_progress(pb)
   tmp
 })
 
-saveRDS(json_peaks, "data/data_output/lung_data/lung_xcalibur_peaks.rds")
+saveRDS(msnbase_peaks, "data/data_output/lung_data/lung_msnbase_peaks.rds")
